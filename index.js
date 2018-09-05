eea3384a6d8af2a4210c8f514271bbd3628bd1d5
Dhruvin Patel <42991798+DhruvinPateel@users.noreply.github.com>
2018-09-05 15:06:07 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var questionBank = 
+[
+  {
+    "question": "What type of language is PHP?", 
+    "answers": {
+      "a":"Front end scripting language",
+      "b":"Server side language"
+    },
+    "correct":"b",
+  },
+  {
+    "question": "In CSS, what property should be used to create a 20px space above and below elements?", 
+    "answers": {
+      "a":"cell-padding: 20px;",
+      "b":"margin:20px;"
+    },
+    "correct":"b",
+  },
+  {
+    "question": "Which code is used to create variables in PHP?", 
+    "answers": {
+      "a":"$x = 45;",
+      "b":"let m = 45;"
+    },
+    "correct":"a",
+  },
+  {
+    "question": "In PHP, how to output the contents of an array?", 
+    "answers": {
+      "a":"print_r",
+      "b":"echo"
+    },
+    "correct":"a",
+  },
+  {
+    "question": "What is the LAMP stack?", 
+    "answers": {
+      "a":"An HTML preprocessor",
+      "b":"Linux, Apache, MySQL, PHP"
+    },
+    "correct":"b",
+  }
+]
